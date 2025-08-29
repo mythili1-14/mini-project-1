139f0cbfe69c4c295e064477c3c68008d7d8c484
Ubuntu <ubuntu@ip-172-31-38-103.ap-south-1.compute.internal>
2025-08-29 10:45:59 +0000

    Deploy files and scripts for EKS CI/CD pipeline

---
+++ script/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "Updating kubeconfig..."
+aws eks update-kubeconfig --region ap-south-1 --name brain-tasks-cluster
+
+echo "Applying Kubernetes manifests..."
+kubectl apply -f /app/k8s/deployment.yaml
+kubectl apply -f /app/k8s/service.yaml
+
+echo "Deployment completed."
