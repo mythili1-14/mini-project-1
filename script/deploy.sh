058ea04ad0f98239a3c0ced13444e88fb260664a
Ubuntu <ubuntu@ip-172-31-38-103.ap-south-1.compute.internal>
2025-08-31 13:28:12 +0000

    changes

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,6 +5,17 @@ set -e
 echo "Updating kubeconfig..."
 aws eks update-kubeconfig --region ap-south-1 --name brain-tasks-app-cluster
 
+# Annotate the AWS Load Balancer Controller service account
+ROLE_ARN="arn:aws:iam::551210489378:role/AWSLoadBalancerControllerIAMPolicy"
+
+kubectl annotate serviceaccount \
+  -n kube-system aws-load-balancer-controller \
+  eks.amazonaws.com/role-arn=$ROLE_ARN \
+  --overwrite
+
+# Restart the ALB controller to pick up the new IAM role
+kubectl rollout restart deployment aws-load-balancer-controller -n kube-system
+
 echo "Applying Kubernetes manifests..."
 kubectl apply -f /app/k8s/deployment.yaml
 kubectl apply -f /app/k8s/service.yaml
